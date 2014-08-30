4e05cd99737f247ccb6d6c938fda28110edb5bf5
matthew <matthew@epiphanydesign.co>
2014-08-29 21:19:38 -0500

    fixxxxxxxx

--- wp-content/themes/fancy/functions.php
+++ wp-content/themes/fancy/functions.php
@@ -30,6 +30,10 @@ function custom_excerpt_length( $length ) {
 }
 add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
+function new_excerpt_more( $more ) {
+	return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . __('Read More', 'your-text-domain') . '</a>';
+}
+add_filter( 'excerpt_more', 'new_excerpt_more' );
 
 add_theme_support( 'post-thumbnails' ); 
 
